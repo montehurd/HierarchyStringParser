06ff90e04e9a46c0448db6bce59c50e4edbc7765
montehurd <mhurd@wikimedia.org>
2025-01-13 18:55:55 -0600

    Add example usage for methods

--- SampleProject/Sources/main.swift
+++ SampleProject/Sources/main.swift
@@ -69,3 +69,19 @@ colors
 let elements = parseHierarchyString(input)
 
 print(elements.description)
+
+print("\n=== Root Level Elements ===")
+print(elements.childrenOfIndex(-1))
+
+print("\n=== Children of 'foods' (index 16) ===")
+print(elements.childrenOfIndex(16))
+
+print("\n=== Siblings of 'wheat' bread (index 19) ===")
+print(elements.siblingsOfIndex(19))
+
+print("\n=== Ancestry of 'walleye' (index 46) ===")
+print(elements.ancestryOfIndex(46))
+
+print("\n=== Descendant Relationship Checks ===")
+print("Is 'bass' (index 49) a descendant of 'fishing' (index 44)? \(elements.isIndex(49, descendantOfIndex: 44))")
+print("Is 'fried' (index 15) a descendant of first 'vegetables' (index 8)? \(elements.isIndex(15, descendantOfIndex: 8))")
