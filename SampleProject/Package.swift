19be1a49bbbb23406bc85c8f08f86ed04668bbb5
montehurd <mhurd@wikimedia.org>
2025-01-13 18:39:12 -0600

    Update sample project to 1.0.1

--- SampleProject/Package.swift
+++ SampleProject/Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "HierarchyTest",
     dependencies: [
-       .package(url: "https://github.com/montehurd/HierarchyStringParser.git", from: "1.0.0")
+       .package(url: "https://github.com/montehurd/HierarchyStringParser.git", from: "1.0.1")
     ],
     targets: [
         .executableTarget(
